b6d70363837118172fb63a09d8d29ac6e3a3154c
DI-TimDavenport <timdavenport@developersinstitute.co.nz>
2022-02-22 17:34:07 +1300

    Final

--- src/components/Welcome/WelcomeImage.jsx
+++ src/components/Welcome/WelcomeImage.jsx
@@ -17,7 +17,7 @@ const WelcomeImage = () => {
   return (
 
       <Wrapper id='module' >
-            <img className="bannerimage" src="https://imagedelivery.net/RT_RPMu352DnmQo9WAcrJw/6e48da32-3d06-466d-9232-b161c6854e00/banner" alt="hustlas"></img>
+            <img className="bannerimage" src="https://imagedelivery.net/RT_RPMu352DnmQo9WAcrJw/6e48da32-3d06-466d-9232-b161c6854e00/backgrounddesktop" alt="hustlas"></img>
         </Wrapper>
  
   );
