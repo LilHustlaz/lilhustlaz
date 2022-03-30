cdeb96ebd5d67fd3271973814d16b9db36b5c890
Syndicure <99310840+TheSyndicure@users.noreply.github.com>
2022-03-30 16:04:48 +1300

    Update RoadmapText.jsx

--- src/components/Roadmap/RoadmapText.jsx
+++ src/components/Roadmap/RoadmapText.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const RoadmapText = () => {
   return (
     <Wrapper>
-      <WelcomeTitle className="welcome-title">EARNING PROGRAM</WelcomeTitle>
+      <WelcomeTitle className="welcome-title">EARNING PROGRAMS</WelcomeTitle>
     </Wrapper>
   );
 };
