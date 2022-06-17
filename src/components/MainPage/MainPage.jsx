b3e45e848f0f22281ab69af48173625fcf5d8c97
LilHustlaz <95151820+LilHustlaz@users.noreply.github.com>
2022-06-17 13:54:52 -0400

    Removing team

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -6,7 +6,6 @@ import Swag from "../Swag/AboutSwag";
 import Breeding from "../Breeding/AboutBreeding";
 import UltraRares from "../UltraRares/UltraRares";
 import Roadmap from "../Roadmap/Roadmap";
-import TheTeam from "../TheTeam/TheTeam";
 import NFTLinks from "../NFTLinks/NFTLinks";
 import FAQ from "../FAQ/FAQ";
 import FooterBar from "../Footer/FooterComponent";
